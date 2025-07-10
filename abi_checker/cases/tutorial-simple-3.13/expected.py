1efeda41623a6eea2467a3920f828044c8dfe93f
Petr Viktorin <encukou@gmail.com>
2025-07-10 17:14:01 +0200

    Update expected version compatibility

--- abi_checker/cases/tutorial-simple-3.13/expected.py
+++ abi_checker/cases/tutorial-simple-3.13/expected.py
@@ -12,5 +12,8 @@ if is_limited_api and limited_api < v(3, 10):
     else:
         raise ExpectFailure('needs 3.10 for PyModule_AddObjectRef')
 
+if is_limited_api and limited_api < v(3, 5):
+    raise ExpectFailure('needs limited API 3.5 for PyModuleDef_Init')
+
 if ('t' in compile_features) ^ ('t' in exec_features):
     raise ExpectFailure('gil/free-threading must match')
